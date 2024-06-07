257cba057afb9888bc4825ea29bb098bda3c4ad5
Nicolai Waniek <rochus@rochus.net>
2024-06-07 13:35:28 +0200

    removed hexdump from ncr_numpy_impl_libzip.cpp

--- ncr/ncr_numpy_impl_libzip.cpp
+++ ncr/ncr_numpy_impl_libzip.cpp
@@ -180,35 +180,6 @@ release(backend **bptr)
 	return result::ok;
 }
 
-void
-hexdump(std::ostream& os, const u8_vector &data)
-{
-	const size_t bytes_per_line = 16;
-	for (size_t offset = 0; offset < data.size(); offset += bytes_per_line) {
-		os << std::setw(8) << std::setfill('0') << std::hex << offset << ": ";
-		for (size_t i = 0; i < bytes_per_line; ++i) {
-			// missing bytes will be replaced with whitespace
-			if (offset + i < data.size())
-				os << std::setw(2) << std::setfill('0') << std::hex << static_cast<i32>(data[offset + i]) << ' ';
-			else
-				os << "   ";
-		}
-		os << " | ";
-		for (size_t i = 0; i < bytes_per_line; ++i) {
-			if (offset + i >= data.size())
-				break;
-
-			// non-printable characters will be replaced with '.'
-			char c = data[offset + i];
-			if (c < 32 || c > 126)
-				c = '.';
-			os << c;
-		}
-		os << "\n";
-	}
-}
-
-
 
 /*
  * write - write a buffer to a previously open zip archive
